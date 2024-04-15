4f4f75324821e62f719201c9ade44823ce0471ba
valenndy <andyv2048@gmail.com>
2024-04-15 11:15:30 -0500

    Login del doctor hecho

---
+++ registrarDoctor.js
@@ -0,0 +1,31 @@
+const nameInput = document.getElementById('nameInput');
+const emailInput = document.getElementById('emailInput');
+const passwordInput = document.getElementById('passwordInput');
+const signupButton = document.getElementById('signupButton');
+
+//EVENTOS
+signupButton.addEventListener('click', signup);
+
+
+//FUNCIONES
+function signup(){
+    let name = nameInput.value;
+    let password = passInput.value;
+    let email = emailInput.value;
+
+    let user = {
+        name: name,
+        password: password,
+        email: email
+    };
+
+    postUser(user);
+}
+
+async function postUser(user){
+    //Obj a JSON
+    let json = JSON.stringify(user);
+    console.log(json);
+}
+
+

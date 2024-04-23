c2093d6055edfb85daf2b025f6ebb49d2e642056
joseGuerrero2003 <1005935249@u.icesi.edu.co>
2024-04-23 12:34:10 -0500

    cambios para cambiar de rama

--- registrarDoctor.js
+++ registrarDoctor.js
@@ -2,11 +2,13 @@ const nameInput = document.getElementById('nameInput');
 const emailInput = document.getElementById('emailInput');
 const passwordInput = document.getElementById('passwordInput');
 const signupButton = document.getElementById('signupButton');
+const logoutButton = document.getElementById('logoutButton');
 
 const URL_BASE = "http://127.0.0.1:8080"; //cambiar URL 
 
 //EVENTOS
 signupButton.addEventListener('click', signup);
+logoutButton.addEventListener('click', logout);
 
 //Comentario
 
@@ -23,6 +25,22 @@ function signup(){
     };
     console.log(user);
     postUser(user);
+}
+
+//FUNCIONES
+function logout(){
+    axios.get('/logout') // Envía una solicitud GET al endpoint /logout
+        .then(response => {
+            // Maneja la respuesta del servidor
+            console.log(response.data); // Muestra la respuesta del servidor en la consola
+            // Redirige a la página de inicio de sesión u otra página relevante
+            window.location.href = '/registrarDoctor.html'; // Cambia '/login' por la URL de tu página de inicio de sesión
+        })
+        .catch(error => {
+            // Maneja los errores
+            console.error('Error al cerrar sesión:', error);
+            alert('Error al cerrar sesión. Por favor, intenta de nuevo.');
+        });
 }
 
 async function postUser(user){
